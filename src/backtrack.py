b0238789554b1740b5a54327eda12da75651dddc
Josh Caron <joshua@minshane.com>
2026-04-01 20:54:29 -0400

    Implemented backtrack_sequence for part (b) of output

--- src/backtrack.py
+++ src/backtrack.py
@@ -1,2 +1,17 @@
 def backtrack_sequence(dp, a, b, values):
-    ...
+    i, j = len(a), len(b)
+    sequence = []
+
+    while i > 0 and j > 0:
+        # If char contributes to the current DP value, include and move diagonally.
+        if a[i - 1] == b[j - 1] and dp[i][j] == dp[i - 1][j - 1] + values.get(a[i - 1], 0):
+            sequence.append(a[i - 1])
+            i -= 1
+            j -= 1
+        elif dp[i - 1][j] >= dp[i][j - 1]:
+            i -= 1
+        else:
+            j -= 1
+
+    sequence.reverse()
+    return "".join(sequence)
